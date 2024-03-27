23bed8b2f487868214e5ec2bc0dd5ddaf96c6037
dudaricci <131046275+dudaricci@users.noreply.github.com>
2024-03-27 08:14:46 -0300

    .

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ alert('ola, seja bem vindo!')
 let nomeUsuario = '';
 let texto = document.querySelector('span');
 
-texto.textContent = nomeUsuario;
 while (nomeUsuario == ''){
    nomeUsuario = prompt('qual o seu nome?');
 }
+
+texto.textContent = nomeUsuario;
