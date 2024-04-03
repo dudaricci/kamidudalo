a44972ed499b4ca33ae136d6850ebecf65206d96
dudaricci <131046275+dudaricci@users.noreply.github.com>
2024-04-03 08:41:30 -0300

    .

--- cont.js
+++ cont.js
@@ -7,6 +7,10 @@ let i = 0;
 
 for(let =0;i < botoes.length; i++ ){
 console.log(i);
-botoes[1].classList.add('ativo')
-botoes[0].classList.remove('ativo')
+botoes[i].onclick = function(){
+botoes[i].classList.add('ativo')
+
+}
+
+//botoes[i].classList.remove('ativo')
 }
