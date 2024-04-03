09fca613950ec17f910ea99b591fe1d6af036cc5
dudaricci <131046275+dudaricci@users.noreply.github.com>
2024-04-03 08:46:23 -0300

    .

--- cont.js
+++ cont.js
@@ -1,14 +1,11 @@
 const botoes = document.querySelectorAll('.objetivos');
 // botoes.length
 
-let i = 0;
 
-
-
-for(let =0;i < botoes.length; i++ ){
+for(let i =0;i < botoes.length; i++ ){
 console.log(i);
 botoes[i].onclick = function(){
-botoes[i].classList.add('ativo')
+botoes[i].classList.add('ativo');
 
 }
 
