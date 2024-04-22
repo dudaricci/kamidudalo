697c79c34b41a2ce48e70649f0cb4a8f792478b5
dudaricci <131046275+dudaricci@users.noreply.github.com>
2024-04-22 11:25:32 -0300

    f

--- cont.js
+++ cont.js
@@ -19,7 +19,7 @@ textos[i].classList.add('ativa');
 const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date ('2024-04-16T00:00:00');
 
-let tempoAtual = new Date ();
+let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo1 - TempoAtual;
 let segundos = Math.floor( tempoFinal/1000);
 let minutos = Math.floor(minutos/60);
